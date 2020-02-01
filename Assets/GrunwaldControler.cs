297a510a84561233e2d6c9d570b9446199b0f2ca
Prota <protez95@gmail.com>
2020-02-01 01:49:42 +0100

    GrunwaldColiders

---
+++ Assets/GrunwaldControler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrunwaldControler : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    public void AudioPlay()
+    {
+        audioSource.Play();
+    }
+
+}
